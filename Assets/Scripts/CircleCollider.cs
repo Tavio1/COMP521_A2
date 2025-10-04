e553d53cf1795db1c25d0b209a3e4b1dbfc6e63c
TavioFicaccio <tavio@perfectstorm.gg>
2025-10-04 18:02:05 -0400

    forgor to save

--- Assets/Scripts/CircleCollider.cs
+++ Assets/Scripts/CircleCollider.cs
@@ -8,7 +8,12 @@ public class CircleCollider : ICollider
     public override Collision IntersectsWithCircle(CircleCollider other)
     {
         Vector3 difference = other.transform.position - transform.position;
+
+        // No collision, return null
         if (difference.magnitude > (radius + other.radius)) return null;
+
+        // Create collision
+        
 
         Collision collision = new Collision(this.gameObject, other.gameObject,
             (transform.position + other.transform.position) / 2,
