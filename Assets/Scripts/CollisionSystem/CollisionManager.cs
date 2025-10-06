a971b28d9de3659348a04c5e35fecfb2df87b7fb
TavioFicaccio <tavio@perfectstorm.gg>
2025-10-06 14:09:10 -0400

    Add impulse for rotational velocity of bumpers

--- Assets/Scripts/CollisionSystem/CollisionManager.cs
+++ Assets/Scripts/CollisionSystem/CollisionManager.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollisionManager : MonoBehaviour, IGameSystem
@@ -50,24 +48,13 @@ public class CollisionManager : MonoBehaviour, IGameSystem
             // Collision between pinball and static object
             if (collision.other.GetComponent<ICollider>().isStatic)
             {
-                try
-                {  
-                    RigidBody rb = collision.obj.GetComponent<RigidBody>();
-                    Vector3 vNeg = rb.velocity;
-                    Vector3 vNegNorm = Vector3.Dot(vNeg, collision.normal) * collision.normal;
-                    Vector3 vPosNorm = -vNegNorm;
-                    float j = (1f + collision.other.GetComponent<ICollider>().restitution) * collision.obj.GetComponent<RigidBody>().mass * vNegNorm.magnitude;
-                    Vector3 impulse = j * collision.normal;
-                    collision.obj.GetComponent<RigidBody>().AddImpulse(impulse);
-                    collision.obj.transform.position = collision.point;
-
-
-                    //Debug.Log("Collision at " +  collision.point + " with normal: " + collision.normal + ", j: " + j + ", impulse: " + impulse );
-                }
-                catch
-                {
-                    throw new System.Exception("No RigidBody attached to non-static object");
-                }
+                RigidBody rb = collision.obj.GetComponent<RigidBody>();
+                Vector3 vNeg = rb.velocity;
+                Vector3 vNegNorm = Vector3.Dot(vNeg, collision.normal) * collision.normal;
+                float j = (1f + collision.other.GetComponent<ICollider>().restitution) * collision.obj.GetComponent<RigidBody>().mass * vNegNorm.magnitude;
+                Vector3 impulse = j * collision.normal;
+                collision.obj.GetComponent<RigidBody>().AddImpulse(impulse);
+                collision.obj.transform.position = collision.point;
             }
             // Collision between pinballs
             else if (collision.other.CompareTag("Pinball"))
@@ -79,9 +66,39 @@ public class CollisionManager : MonoBehaviour, IGameSystem
                 Vector3 v2 = rb2.velocity;
 
                 Vector3 v1Prime = v1 - (Vector3.Dot(v1 - v2, collision.normal) * 2 * rb2.mass / (rb1.mass + rb2.mass)) * collision.normal;
-                
+
                 //Debug.Log(rb1.name + " with " + v1Prime);
-                rb1.AddImpulse(v1Prime); 
+                rb1.AddImpulse(v1Prime);
+            }
+            // Collision between pinball and bumper
+            else
+            {
+                RigidBody rb = collision.obj.GetComponent<RigidBody>();
+                RigidBody bRb = collision.other.GetComponentInParent<RigidBody>();
+
+                Vector3 vNeg = rb.velocity;
+                Vector3 vNegNorm = Vector3.Dot(vNeg, collision.normal) * collision.normal;
+                float j = (1f + collision.other.GetComponent<ICollider>().restitution) * collision.obj.GetComponent<RigidBody>().mass * vNegNorm.magnitude;
+                Vector3 impulse = j * collision.normal;
+                collision.obj.GetComponent<RigidBody>().AddImpulse(impulse);
+                collision.obj.transform.position = collision.point;
+
+                if (bRb.angularVelocity != 0f)
+                {
+                    BoxCollider bCol = collision.other.GetComponent<BoxCollider>();
+                    float boxLength = bCol.maxCorner.x - bCol.minCorner.x;
+                    if (collision.normal.Equals(Vector3.zero))
+                        collision.normal = bCol.transform.rotation * Vector3.back;
+
+                    Vector3 perp = new Vector3(-collision.normal.z, 0, collision.normal.x).normalized;
+                    Vector3 difference = collision.point - bRb.transform.position;
+                    float distanceAlongPaddle = Mathf.Abs(Vector3.Dot(difference, perp)) / boxLength;
+
+                    Vector3 impulse2 = distanceAlongPaddle * bRb.angularVelocity * collision.normal;
+                    collision.obj.GetComponent<RigidBody>().AddImpulse(impulse2);
+                    collision.obj.transform.position = collision.point;
+                }
+
             }
         }
 
